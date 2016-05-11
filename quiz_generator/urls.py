905034336c14c911c0d94a999808e6e3a3e4a3e2
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-05-11 17:27:56 +0000

    flot

--- quiz_generator/urls.py
+++ quiz_generator/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'^home/', views.home, name='home')
 ]
--- quiz_generator/views.py
+++ quiz_generator/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h2>HEYYOALA</h2>")
 def home(request):
     return render(request, 'quiz_generator/home.html')
