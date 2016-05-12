6d027a6c48f7feb333b3c7393fb26250a9d7c4d9
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-05-12 17:49:57 +0000

    flot

--- quiz_generator/views.py
+++ quiz_generator/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from urllib.request import urlopen, Request
 import json
+from random import randint
 # Create your views here.
 
 def getReq(_request):
@@ -26,10 +27,22 @@ def getCategory(_request, categoryID):
         question_answers.append(tup)
     return render(_request, 'quiz_generator/home.html', {'questions':question_answers})
 
+
+# TODO make only one API call for each category
 def getQuiz(_request):
-    print('lolol')
     categories = _request.POST.getlist('check[]')
-    number_of_questions = _request.POST.get('spinner')
-    print(number_of_questions)
-    print(categories)
-    return render(_request, 'quiz_generator/questions.html')
+    number_of_questions = int(_request.POST.get('spinner'))
+    counter = 0
+    questions = []
+    for i in range(number_of_questions):
+        response = urlopen(Request("http://jservice.io/api/category" + '?id=' + str(categories[counter])))
+        resp_parsed = json.loads(response.read().decode())
+        clues = resp_parsed['clues']
+        rand = randint(0,len(clues))
+        tup = (clues[rand]['question'], clues[rand]['answer'])
+        questions.append(tup)
+        if counter == len(categories) - 1:
+            counter = 0
+        else :
+            counter += 1
+    return render(_request, 'quiz_generator/questions.html', {'questions':questions})
