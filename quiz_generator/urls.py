e5465a6719fce74975d9f35fee8c8bdff825d726
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-05-12 14:01:35 +0000

    flot

--- quiz_generator/urls.py
+++ quiz_generator/urls.py
@@ -4,7 +4,13 @@ from quiz_generator.models import Question
 from . import views
 
 urlpatterns = [
-    url(r'^home', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
+    url(r'^home/$', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
                                    template_name="quiz_generator/questions.html")),
-    # url(r'^', views.home, name='home')
+    url(r'^$', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
+                                     template_name="quiz_generator/questions.html")),
+    url(r'^$', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
+                                template_name="quiz_generator/questions.html")),
+    url(r'^home/(?P<pk>\d+)$', DetailView.as_view(model = Question,
+                                             template_name='quiz_generator/question.html')),
+    url(r'^test/$', views.getReq, name='getReq')
 ]
--- quiz_generator/views.py
+++ quiz_generator/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from urllib.request import urlopen, Request
+import json
 # Create your views here.
 
 # def home(request):
@@ -8,3 +9,13 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'quiz_generator/home.html', {'content':['If you bblablabla', 'Another text woohoo']})
+
+def getReq(_request):
+        req = Request("http://jservice.io/api/clues")
+        response = urlopen(req)
+        resp_parsed = json.loads(response.read().decode())
+        questions_answers = []
+        for resp in resp_parsed:
+            tup = (resp['question'], resp['answer'])
+            questions_answers.append(tup)
+        return render(_request, 'quiz_generator/home.html', {'questions':questions_answers})
