512ead3391acc32fc628c2701d37abcca8363239
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-05-12 15:10:16 +0000

    flot

--- quiz_generator/admin.py
+++ quiz_generator/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-
-from quiz_generator.models import Question
-
-
-admin.site.register(Question)
--- quiz_generator/urls.py
+++ quiz_generator/urls.py
@@ -1,16 +1,8 @@
 from django.conf.urls import url
 from django.views.generic import ListView, DetailView
-from quiz_generator.models import Question
 from . import views
 
 urlpatterns = [
-    url(r'^home/$', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
-                                   template_name="quiz_generator/questions.html")),
-    url(r'^$', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
-                                     template_name="quiz_generator/questions.html")),
-    url(r'^$', ListView.as_view(queryset=Question.objects.all().order_by("question_ID")[:25],
-                                template_name="quiz_generator/questions.html")),
-    url(r'^home/(?P<pk>\d+)$', DetailView.as_view(model = Question,
-                                             template_name='quiz_generator/question.html')),
-    url(r'^test/$', views.getReq, name='getReq')
+    url(r'^home/$', views.getReq, name='getReq'),
+    url(r'^home/(?P<categoryID>\d+)$', views.getCategory)
 ]
