23877419e03a14fcc0501a510b99edb385d95e1c
vala14 <vala14@ru.is>
2016-05-13 15:08:25 +0000

    flot

--- quiz_generator/static/quiz_generator/js/scripts.js
+++ quiz_generator/static/quiz_generator/js/scripts.js
@@ -1,3 +1,17 @@
 /**
  * Created by eysteinn on 12/05/16.
  */
+
+function hide_answers() {
+    $(".answer").addClass("hide");
+    $(".answer").removeClass("show");
+    $(".line").addClass("show");
+    $(".line").removeClass("hide");
+}
+
+function show_answers() {
+    $(".answer").removeClass("hide");
+    $(".answer").addClass("show");
+    $(".line").addClass("hide");
+    $(".line").removeClass("show");
+}
